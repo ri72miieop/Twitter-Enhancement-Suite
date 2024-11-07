60e5f8c49b1bbe6057ceb20176916f78b30faa02
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-07 23:16:02 +0000

    improvements on highlight tab to change the user depending on the page the tab is at (currently it's not changing automatically, only by forcing a change in the tabs)

--- tabs/HighlightsTab.tsx
+++ tabs/HighlightsTab.tsx
@@ -1,18 +1,73 @@
 import { useState, useEffect } from "react"
 import AccountTopTweetsClient from "~components/AccountTopTweetsClient"
+import PageChangeDetector from "~components/PageChanger"
 import Tweet from "~components/Tweet"
 import TweetList from "~components/TweetList"
 import { supabase } from "~core/supabase"
 import { fetchTopTweetsByUser } from "~utils/dbUtils"
+import { extractXUsername } from "~utils/TwitterUtils"
+
 
 function HighlightsTab() {
     const [topTweets, setTopTweets] = useState<any[]>(null)
+    const [url, setUrl] = useState<string>(null)
+    const [username, setUsername] = useState<string>(null)
+    const [usernameExistsInCA, setUsernameExistsInCA] = useState<boolean|null>(null)
+
+    useEffect(() =>{
+
+      async function updateUsername(){
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          const currentTab = tabs[0];
+          const currentUrl = currentTab.url;
+          const currentTitle = currentTab.title;
+      
+          console.log(`Current URL: ${currentUrl}`);
+
+          const username = extractXUsername(currentUrl)
+          setUsername(username);
+          setUrl(currentUrl)
+          console.log(`Current Title: ${currentTitle}`);
+      });
+        
+      }
+
+      updateUsername()
+      console.log("updateUsername",window.location)
+
+    },[window.location])
+
+
+
+    useEffect(()=>{
+
+      async function checkUsernameExistsInCA(){
+        if(username){
+          const {data,error} = await supabase.from('account').select('account_id,username,account_display_name').eq('username',username).single()
+          if(data && !error){
+            setUsernameExistsInCA(true)
+            console.log("username exists in CA")
+          }
+          else{
+            setUsernameExistsInCA(false)
+            console.log("username doesn't exist in CA")
+          }
+        }
+      }
+      checkUsernameExistsInCA()
+
+    },[username])
 
     useEffect(() => {
 
         async function fetchData() {
+          
+
+
+          console.log(JSON.stringify(url, null, 2))
             console.log("fetching data")
-            const data = await fetchTopTweetsByUser(supabase, "IaimforGOAT")
+            const data = await fetchTopTweetsByUser(username)
             console.log(data)
             const tweetData = {
                 // liked: data.most_liked_tweets_by_archive_users,
@@ -22,7 +77,7 @@ function HighlightsTab() {
             } 
             
             
-            const {data:account} = await supabase.from('account').select('account_id,username,account_display_name').eq('account_id','345709253');
+            const {data:account} = await supabase.from('account').select('account_id,username,account_display_name').eq('username',username);
             const {data:profile} = await supabase.from('profile').select('*').eq('account_id',account[0].account_id);
 
             console.log(tweetData.favorited);
@@ -39,9 +94,10 @@ function HighlightsTab() {
 
             
         }
-
+        if(usernameExistsInCA != null && usernameExistsInCA){
         fetchData()
-    }, [])
+        }
+    }, [usernameExistsInCA])
 
     
 // <AccountTopTweetsClient
@@ -52,7 +108,11 @@ function HighlightsTab() {
 // />
 
   return (<>
-  {topTweets && topTweets.map((tweet: any) => (
+  
+  {usernameExistsInCA!= null && !usernameExistsInCA &&(
+<span>Sorry, this user didn't add his archive to the Community Archive yet.</span>
+  )}
+  {usernameExistsInCA &&topTweets && topTweets.map((tweet: any) => (
     <Tweet key={tweet.tweet_id} tweet={tweet} />
   ))}
   </>)
