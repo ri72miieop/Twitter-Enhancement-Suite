02885b7b7110462edb584d43cdb80c68a97504a8
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-11 16:28:46 +0000

    verify if user is signed in to make request

--- tabs/OnThisDay.tsx
+++ tabs/OnThisDay.tsx
@@ -1,31 +1,45 @@
+import { useEffect, useState } from "react"
 
-import { useEffect,useState } from "react"
 import Tweet from "~components/Tweet"
 import { supabase } from "~core/supabase"
+import { getUser } from "~utils/dbUtils"
 
 function OnThisDay() {
-        
-    const [tweets, setTweets] = useState<any[]>([])
+  const [tweets, setTweets] = useState<any[]>([])
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false)
 
-    useEffect(() => {
-        async function fetchData() {
-            const { data, error } = await supabase.rpc("get_tweets_on_this_day", {p_account_id:'345709253',p_limit:100})
-            setTweets(data)
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const user = await getUser()
+      if (!user || !user.id) {
+        setIsUserSignedIn(false)
+        return
+      } else {
+        setIsUserSignedIn(true)
+      }
+      const { data, error } = await supabase.rpc("get_tweets_on_this_day", {
+        p_account_id: user.id,
+        p_limit: 100
+      })
+      setTweets(data)
+    }
 
-        fetchData()
-    }, [])
-    
-    return (
-    <>{tweets.length}
-        {tweets && tweets.map((tweet: any) => (
-            <Tweet key={tweet.tweet_id} tweet={tweet} />
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      {isUserSignedIn &&
+        tweets.length > 0 &&
+        tweets.map((tweet: any) => (
+          <Tweet key={tweet.tweet_id} tweet={tweet} />
         ))}
-
+      {!isUserSignedIn && <div>Please sign in to view this page.</div>}
+      {isUserSignedIn && tweets.length === 0 && (
+        <div>No tweets found for this day.</div>
+      )}
     </>
-    )
+  )
 }
-
-
 
 export default OnThisDay
