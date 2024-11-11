a8e855bf0daaab0cc07520821c264b9be4c145b2
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-11 21:39:33 +0000

    Auto-updates when opening new tweets

--- tabs/HighlightsTab.tsx
+++ tabs/HighlightsTab.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
+
 import AccountTopTweetsClient from "~components/AccountTopTweetsClient"
 import PageChangeDetector from "~components/PageChanger"
 import Tweet from "~components/Tweet"
@@ -8,115 +9,140 @@ import { fetchTopTweetsByUser } from "~utils/dbUtils"
 import { DevLog } from "~utils/devUtils"
 import { extractXUsername } from "~utils/TwitterUtils"
 
-
 function HighlightsTab() {
-    const [topTweets, setTopTweets] = useState<any[]>(null)
-    const [url, setUrl] = useState<string>(null)
-    const [username, setUsername] = useState<string>(null)
-    const [usernameExistsInCA, setUsernameExistsInCA] = useState<boolean|null>(null)
+  const [topTweets, setTopTweets] = useState<any[]>(null)
+  const [url, setUrl] = useState<string>(null)
+  const [username, setUsername] = useState<string>(null)
+  const [usernameExistsInCA, setUsernameExistsInCA] = useState<boolean | null>(
+    null
+  )
 
-    useEffect(() =>{
+  useEffect(() => {
+    const updateUsername = async () => {
+      // Get the active tab in the current window
+      const tabs = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      })
+      const currentTab = tabs[0]
 
-      async function updateUsername(){
+      if (currentTab?.url) {
+        const currentUrl = currentTab.url
+        const currentTitle = currentTab.title
 
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const currentTab = tabs[0];
-          const currentUrl = currentTab.url;
-          const currentTitle = currentTab.title;
-      
-          DevLog(`Current URL: ${currentUrl}`);
-
-          const username = extractXUsername(currentUrl)
-          setUsername(username);
-          setUrl(currentUrl)
-          DevLog(`Current Title: ${currentTitle}`);
-      });
-        
+        DevLog(`Initial URL: ${currentUrl}`)
+        const username = extractXUsername(currentUrl)
+        setUsername(username)
+        setUrl(currentUrl)
       }
+    }
 
-      updateUsername()
-      DevLog("updateUsername" + window.location)
+    const updateUsernameFromUrl = async (url: string) => {
+      DevLog(`Current URL: ${url}`)
+      const username = extractXUsername(url)
+      setUsername(username)
+      setUrl(url)
+    }
 
-    },[window.location])
+    // Initial update
+    updateUsername()
 
+    // Listen for tab updates
+    const tabUpdateListener = (tabId, changeInfo, tab) => {
+      if (changeInfo.url) {
+        updateUsernameFromUrl(changeInfo.url)
+      }
+    }
 
+    chrome.tabs.onUpdated.addListener(tabUpdateListener)
 
-    useEffect(()=>{
+    return () => {
+      chrome.tabs.onUpdated.removeListener(tabUpdateListener)
+    }
+  }, [])
 
-      async function checkUsernameExistsInCA(){
-        if(username){
-          const {data,error} = await supabase.from('account').select('account_id,username,account_display_name').eq('username',username).single()
-          if(data && !error){
-            setUsernameExistsInCA(true)
-            DevLog("username exists in CA")
-          }
-          else{
-            setUsernameExistsInCA(false)
-            DevLog("username doesn't exist in CA")
-          }
+  useEffect(() => {
+    async function checkUsernameExistsInCA() {
+      if (username) {
+        const { data, error } = await supabase
+          .from("account")
+          .select("account_id,username,account_display_name")
+          .ilike("username", username)
+          .single()
+        if (data && !error) {
+          setUsernameExistsInCA(true)
+          DevLog("username exists in CA")
+          return true;
+        } else {
+          setUsernameExistsInCA(false)
+          DevLog("username doesn't exist in CA")
+          return false;
         }
       }
-      checkUsernameExistsInCA()
-
-    },[username])
-
-    useEffect(() => {
-
-        async function fetchData() {
-          
-
-
-          DevLog(JSON.stringify(url, null, 2))
-            DevLog("fetching data")
-            const data = await fetchTopTweetsByUser(username)
-            DevLog(data)
-            const tweetData = {
-                // liked: data.most_liked_tweets_by_archive_users,
-                // replied: data.most_replied_tweets_by_archive_users,
-                favorited: data.most_favorited_tweets,
-                retweeted: data.most_retweeted_tweets,
-            } 
-            
-            
-            const {data:account} = await supabase.from('account').select('account_id,username,account_display_name').eq('username',username);
-            const {data:profile} = await supabase.from('profile').select('*').eq('account_id',account[0].account_id);
-
-            DevLog("tweetData.favorited " + JSON.stringify(tweetData.favorited));
-            for(let i = 0; i < tweetData.favorited.length; i++){
-
-              tweetData.favorited[i].avatar_media_url = profile[0].avatar_media_url;
-              tweetData.favorited[i].username = account[0].username;
-              tweetData.favorited[i].account_display_name = account[0].account_display_name;
-            }
-
-
-            setTopTweets(tweetData.favorited)
-            
-
-            
-        }
-        if(usernameExistsInCA != null && usernameExistsInCA){
-        fetchData()
-        }
-    }, [usernameExistsInCA])
-
+    }
     
-// <AccountTopTweetsClient
-// tweetData={topTweets}
-// username="IaimforGOAT"
-// displayName="IaimforGOAT"
-// profilePicUrl="https://pbs.twimg.com/profile_images/1712168250441146368/4Fg5g55a_400x400.jpg"
-// />
-
-  return (<>
   
-  {usernameExistsInCA!= null && !usernameExistsInCA &&(
-<span>Sorry, this user didn't add his archive to the Community Archive yet.</span>
-  )}
-  {usernameExistsInCA &&topTweets && topTweets.map((tweet: any) => (
-    <Tweet key={tweet.tweet_id} tweet={tweet} />
-  ))}
-  </>)
+    async function fetchData() {
+      if (!username) return
+      DevLog(JSON.stringify(url, null, 2))
+      DevLog("fetching data")
+      const data = await fetchTopTweetsByUser(username)
+      DevLog(data)
+      const tweetData = {
+        // liked: data.most_liked_tweets_by_archive_users,
+        // replied: data.most_replied_tweets_by_archive_users,
+        favorited: data.most_favorited_tweets,
+        retweeted: data.most_retweeted_tweets
+      }
+
+      const { data: account } = await supabase
+        .from("account")
+        .select("account_id,username,account_display_name")
+        .eq("username", username)
+      const { data: profile } = await supabase
+        .from("profile")
+        .select("*")
+        .eq("account_id", account[0].account_id)
+
+      for (let i = 0; i < tweetData.favorited.length; i++) {
+        tweetData.favorited[i].avatar_media_url = profile[0].avatar_media_url
+        tweetData.favorited[i].username = account[0].username
+        tweetData.favorited[i].account_display_name =
+          account[0].account_display_name
+      }
+
+      setTopTweets(tweetData.favorited)
+    }
+
+    checkUsernameExistsInCA().then((exists) => {
+      if (exists) {
+        fetchData()
+      }
+    })
+  }, [username])
+
+  // <AccountTopTweetsClient
+  // tweetData={topTweets}
+  // username="IaimforGOAT"
+  // displayName="IaimforGOAT"
+  // profilePicUrl="https://pbs.twimg.com/profile_images/1712168250441146368/4Fg5g55a_400x400.jpg"
+  // />
+
+  return (
+    <>
+      {usernameExistsInCA != null && !usernameExistsInCA && username && (
+        <span>
+          Sorry, <b>@{username}</b> didn't add their archive to the Community Archive
+          yet.
+        </span>
+      )}
+      {usernameExistsInCA &&
+        topTweets &&
+        topTweets.map((tweet: any) => (
+          <Tweet key={tweet.tweet_id} tweet={tweet} />
+        ))}
+    </>
+  )
 }
 
 export default HighlightsTab
