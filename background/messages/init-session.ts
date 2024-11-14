8537b07649ff1264c217d66212e6572fec831a85
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-14 22:24:33 +0000

    use DevLog

--- background/messages/init-session.ts
+++ background/messages/init-session.ts
@@ -1,10 +1,11 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
 import { supabase } from "~core/supabase"
+import { DevLog } from "~utils/devUtils"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   supabase.auth.onAuthStateChange((event, session) => {
-    console.log(event, session)
+    DevLog(event, session)
   })
 
   await supabase.auth.setSession(req.body)
