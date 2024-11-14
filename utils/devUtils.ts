ac4bb70adb04d6810852bbee6de25b97fac9cede
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-14 22:25:16 +0000

    allow DevLog to receive variable arguments

--- utils/devUtils.ts
+++ utils/devUtils.ts
@@ -1,4 +1,10 @@
-export const DevLog = (message: string, type: "info" | "warn" | "error" | "debug" = "info") => {
+export const DevLog = (...args: any[]) => {
+  const type = typeof args[args.length-1] === 'string' && ["info", "warn", "error", "debug"].includes(args[args.length-1]) 
+    ? args.pop() as "info" | "warn" | "error" | "debug"
+    : "info";
+  const message = args.map(arg => 
+    typeof arg === 'string' ? arg : JSON.stringify(arg)
+  ).join(' ');
   if (process.env.NODE_ENV === "development") {
     switch (type) {
       case "warn":
