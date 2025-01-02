638b4f2601d616411ec778107c0e427c1f954b4d
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-01-02 14:11:10 +0000

    implemented interceptor for search timeline

--- InterceptorModules/search-timeline.ts
+++ InterceptorModules/search-timeline.ts
@@ -1,6 +1,6 @@
-import { extractDataFromResponse, extractTimelineTweet } from "~utils/twe_utils";
+import { extractDataFromResponse, extractTimelineTweet, extractTimelineUser, isTimelineEntryListSearch, isTimelineEntrySearchGrid, isTimelineEntryTweet, isTimelineEntryUser } from "~utils/twe_utils";
 import type { Interceptor } from "./types/General";
-import type { TimelineInstructions, Tweet } from "./types";
+import type { ItemContentUnion, List, TimelineAddEntriesInstruction, TimelineAddToModuleInstruction, TimelineInstructions, TimelineTweet, TimelineTwitterList, Tweet, User } from "./types";
 import { DevLog } from "~utils/devUtils";
 
 
@@ -8,8 +8,10 @@ interface SearchTimelineResponse {
   data: {
     search_by_raw_query: {
       search_timeline: {
-        instructions: TimelineInstructions;
-        responseObjects: unknown;
+        timeline: {
+          instructions: TimelineInstructions;
+          responseObjects: unknown;
+        };
       };
     };
   };
@@ -23,22 +25,96 @@ export const SearchTimelineInterceptor: Interceptor = (req, res) => {
   }
 
   try {
-    const newData = extractDataFromResponse<SearchTimelineResponse, Tweet>(
-      res,
-      (json) => json.data.search_by_raw_query.search_timeline.instructions,
-      (entry) => extractTimelineTweet(entry.content.itemContent),
-    );
-    DevLog("Interceptor.function - SearchTimelineInterceptor: ", newData.length)
+    const json: SearchTimelineResponse = JSON.parse(res.responseText);
+    const instructions = json.data.search_by_raw_query.search_timeline.timeline.instructions;
+
+    // Parse tweets in search results.
+    // Currently, only "Top", "Latest" and "Media" are supported. "People" and "Lists" are ignored.
+    const newTweets: Tweet[] = [];
+    //const newUsers: User[] = [];
+    //const newLists: List[] = [];
+
+    // The most complicated part starts here.
+    //
+    // For "Top" and "Latest", the "TimelineAddEntries" instruction contains normal tweets.
+    // For "People", the "TimelineAddEntries" instruction contains normal users.
+    // For "Media", the "TimelineAddEntries" instruction initializes "search-grid" module.
+    // For "Lists", the "TimelineAddToModule" instruction initializes "list-search" module.
+    const timelineAddEntriesInstruction = instructions.find(
+      (i) => i.type === 'TimelineAddEntries',
+    ) as TimelineAddEntriesInstruction<ItemContentUnion>;
+
+    // There will be two requests for "Media" and "Lists" search results.
+    // The "TimelineAddToModule" instruction then prepends items to existing module.
+    const timelineAddToModuleInstruction = instructions.find(
+      (i) => i.type === 'TimelineAddToModule',
+    ) as TimelineAddToModuleInstruction<ItemContentUnion>;
+
+    // The "TimelineAddEntries" instruction may not exist in some cases.
+    const timelineAddEntriesInstructionEntries = timelineAddEntriesInstruction?.entries ?? [];
+
+    // First, parse "TimelineAddEntries" instruction.
+    for (const entry of timelineAddEntriesInstructionEntries) {
+      // Extract normal tweets.
+      if (isTimelineEntryTweet(entry)) {
+        const tweet = extractTimelineTweet(entry.content.itemContent);
+        if (tweet) {
+          newTweets.push(tweet);
+        }
+      }
+
+      // Extract media tweets.
+      if (isTimelineEntrySearchGrid(entry)) {
+        const tweetsInSearchGrid = entry.content.items
+          .map((i) => extractTimelineTweet(i.item.itemContent))
+          .filter((t): t is Tweet => !!t);
+
+        newTweets.push(...tweetsInSearchGrid);
+      }
+
+      //// Extract users.
+      //if (isTimelineEntryUser(entry)) {
+      //  const user = extractTimelineUser(entry.content.itemContent);
+      //  if (user) {
+      //    newUsers.push(user);
+      //  }
+      //}
+
+      //// Extract lists.
+      //if (isTimelineEntryListSearch(entry)) {
+      //  const lists = entry.content.items.map((i) => i.item.itemContent.list);
+      //  newLists.push(...lists);
+      //}
+    }
+
+    // Second, parse "TimelineAddToModule" instruction.
+    if (timelineAddToModuleInstruction) {
+      const items = timelineAddToModuleInstruction.moduleItems.map((i) => i.item.itemContent);
+
+      const tweets = items
+        .filter((i): i is TimelineTweet => i.__typename === 'TimelineTweet')
+        .map((t) => extractTimelineTweet(t))
+        .filter((t): t is Tweet => !!t);
+
+      newTweets.push(...tweets);
+
+      //const lists = items
+      //  .filter((i): i is TimelineTwitterList => i.__typename === 'TimelineTwitterList')
+      //  .map((i) => i.list);
+//
+      //newLists.push(...lists);
+    }
+    DevLog("Interceptor.function - SearchTimelineInterceptor: ", newTweets.length)
     
     // Dispatch a custom event
-    for(const tweet of newData) {
+    for(const tweet of newTweets) {
       DevLog("Sending intercepted data to IndexDB:", tweet.rest_id)
       window.dispatchEvent(new CustomEvent('dataInterceptedEvent', { detail: {data:tweet, type: "search-timeline", originator_id: tweet.rest_id, item_id: tweet.rest_id }}));
     }
-    DevLog('TTT HomeTimeline: ', JSON.stringify(newData, null, 2))
-    DevLog(`TTT HomeTimeline: ${newData.length} items received`);
+    DevLog('TTT SearchTimeline: ', JSON.stringify(newTweets, null, 2))
+    DevLog(`TTT SearchTimeline: ${newTweets.length} items received`);
   } catch (err) {
-    DevLog('TTT HomeTimeline: Failed to parse API response', err)
+    DevLog('TTT SearchTimeline: Failed to parse API response', err)
     //logger.debug(req.method, req.url, res.status, res.responseText);
     //logger.errorWithBanner('HomeTimeline: Failed to parse API response', err as Error);
   }
