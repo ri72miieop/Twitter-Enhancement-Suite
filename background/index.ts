93a2e0583adcb7c3f7fa0a78a358a921337baf55
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-03-03 22:05:19 +0000

    update background script to include old message (it's cute!)

--- background/index.ts
+++ background/index.ts
@@ -3,3 +3,8 @@ export {}
 chrome.action.onClicked.addListener(() => {
   chrome.runtime.openOptionsPage()
 })
+
+
+console.log(
+  "Live now; make now always the most precious time. Now will never come again."
+)
