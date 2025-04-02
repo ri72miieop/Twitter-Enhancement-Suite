b1d8c30bf2d48b1dc117586a4cb10301ade27653
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-04-02 18:41:57 +0100

    add error boundary and some debug components on dev

--- options.tsx
+++ options.tsx
@@ -1,10 +1,32 @@
-import SignIn from "~components/SignIn"
+import DebugTimelineProcessor from "~components/debug-timeline-processor"
+import ErrorBoundary from "~components/errors/ErrorBoundary"
+import { IndexDbDuplicator } from "~components/IndexDbDuplicator"
 import InterceptorDashboard from "~components/InterceptorDashboard"
+import SignIn from "~components/SignIn"
+import { isDev } from "~utils/devUtils"
+
+import "~/prod.css"
 
 function IndexOptions() {
-  return (<><SignIn />
-  <InterceptorDashboard /></>)
-}
+  
+  return (
+    <>
+      <SignIn />
+      <ErrorBoundary>
+        <InterceptorDashboard />
+      </ErrorBoundary>
 
+      {isDev && (
+        <>
+          <ErrorBoundary>
+            <IndexDbDuplicator />
+          </ErrorBoundary>
+
+          <DebugTimelineProcessor />
+        </>
+      )}
+    </>
+  )
+}
 
 export default IndexOptions
